e324daf79c643b94c10deb8c65f6bc14e6fc2d82
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2022-09-19 00:01:27 +0700

    Updates

---
+++ precache-manifest.8a10f8cc0a21d58c7a5158e92f7d9695.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "470a99b6e998175fa8c0",
+    "url": "/FER201_Lab1/static/css/main.8880739d.chunk.css"
+  },
+  {
+    "revision": "470a99b6e998175fa8c0",
+    "url": "/FER201_Lab1/static/js/main.470a99b6.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/FER201_Lab1/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "274396811fff8c54c05e",
+    "url": "/FER201_Lab1/static/js/2.27439681.chunk.js"
+  },
+  {
+    "revision": "f0a29f8f2165ed7fa351",
+    "url": "/FER201_Lab1/static/js/runtime~main.f0a29f8f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/FER201_Lab1/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6207c1abdbaececfad9f7100a9e8175e",
+    "url": "/FER201_Lab1/static/media/photo-1533134486753-c833f0ed4866.6207c1ab.jpg"
+  },
+  {
+    "revision": "b9d2257159e33da60e3f95b2bdcd36ed",
+    "url": "/FER201_Lab1/index.html"
+  }
+];
