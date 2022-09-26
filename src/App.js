b0f217daa09e3fbde4a7c3a1b94bcf23817fb0b1
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2022-09-27 01:29:29 +0700

    lastCommit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import React, { Component } from "react";
 import Navigation from "./components/Navigation";
 import Film from "./Film";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div className="App">
+      <i class="fa-thin fa-house"></i>
       <Navigation/>
       <Film/>
+      
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,55 +4,59 @@ import { ThemeContext } from "./ThemeContext";
 export default function Navigation() {
   const { theme, toggle, dark } = useContext(ThemeContext);
   return (
-    <div className="navbar something">
-      <nav
-        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
-      >
-        <ul className="nav-cont">
-          <li>
-            <a className="active" href="#home" style={{ color: theme.color }}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#review" style={{ color: theme.color }}>
-              Review
-            </a>
-          </li>
-          <li>
-            <a href="#booking" style={{ color: theme.color }}>
-              Booking
-            </a>
-          </li>
-          <li>
-            <a
-              className="contact"
-              href="#Contact"
-              style={{ color: theme.color }}
-            >
-              Contact
-            </a>
-          </li>
-          <li>
-          <div style={{ position: "relative" }}>
-            <a
-              for="theme_toggle"
-              className="switch-mode"
-              href="#"
-              onClick={toggle}
-              style={{
-                backgroundColor: theme.backgroundColor,
-                color: theme.color,
-                outline: "none",
-              }}
-            >
-              Switch to {!dark ? "Dark" : "Light"}
-            </a>
-          </div>
-        </li>
-        </ul>
-        
-      </nav>
+    <div>
+      <div className="toggle_menu">
+        <img src="https://www.vhv.rs/file/max/37/372285_menu-icon-png.jpg" />
+      </div>
+      <div className="navbar something">
+        <nav
+          style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+        >
+          <ul className="nav-cont">
+            <li>
+              <a className="active" href="#home" style={{ color: theme.color }}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#review" style={{ color: theme.color }}>
+                Review
+              </a>
+            </li>
+            <li>
+              <a href="#booking" style={{ color: theme.color }}>
+                Booking
+              </a>
+            </li>
+            <li>
+              <a
+                className="contact"
+                href="#Contact"
+                style={{ color: theme.color }}
+              >
+                Contact
+              </a>
+            </li>
+            <li>
+              <div style={{ position: "relative" }}>
+                <a
+                  for="theme_toggle"
+                  className="switch-mode"
+                  href="#"
+                  onClick={toggle}
+                  style={{
+                    backgroundColor: theme.backgroundColor,
+                    color: theme.color,
+                    outline: "none",
+                  }}
+                >
+                  Switch to {!dark ? "Dark" : "Light"}
+                </a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
